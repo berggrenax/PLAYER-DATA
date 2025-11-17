0cb942c846d5076243443c3b945a447682166d1d
Axel Berggren <berggren.ax@gmail.com>
2025-11-17 17:49:32 +0100

    Superminimal app

--- app.py
+++ app.py
@@ -5,6 +5,3 @@ st.set_page_config(page_title="TEST", layout="wide")
 st.title("Testar att Streamlit funkar i molnet")
 
 st.write("Hej från molnet 👋")
-
-st.write("Nycklar i st.secrets:")
-st.write(list(st.secrets.keys()))
